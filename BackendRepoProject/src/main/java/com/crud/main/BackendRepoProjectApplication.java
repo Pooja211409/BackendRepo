e422a93caff5000b8d4f42a05e6f4b760ab01992
poojamkokane <poojamkokane@gmail.com>
2024-07-04 02:07:37 +0530

    Project Root

---
+++ BackendRepoProject/src/main/java/com/crud/main/BackendRepoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.crud.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRepoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRepoProjectApplication.class, args);
+	}
+
+}
