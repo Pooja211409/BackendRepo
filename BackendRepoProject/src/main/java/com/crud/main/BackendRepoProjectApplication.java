4a9805b7f435015471fad6492b9f7292ce0cf37a
poojamkokane <poojamkokane@gmail.com>
2024-07-04 09:52:29 +0530

    test commit

--- BackendRepoProject/src/main/java/com/crud/main/BackendRepoProjectApplication.java
+++ BackendRepoProject/src/main/java/com/crud/main/BackendRepoProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendRepoProjectApplication {
 
 	public static void main(String[] args) {
-		
+		//test comment
 		SpringApplication.run(BackendRepoProjectApplication.class, args);
 	}
 
